e3bf66d304341bc6eb7d8fbb27ced29258a3c85b
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2025-02-24 23:20:11 -0800

    Runtime: 15 ms (43.42%), Memory: 17.9 MB (63.90%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,25 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): 
+        if len(s) != len(t):
             return False
-        
+
         s_lower = s.lower()
         t_lower = t.lower()
 
-        charHash = [0] * 26
+        charHash = {}
 
         for i in range(len(s_lower)):
-            charHash[ord(s_lower[i]) - ord('a')] += 1
+            charHash[s_lower[i]] = 1 + charHash.get(s_lower[i], 0)
         for i in range(len(t_lower)):
-            charHash[ord(t_lower[i]) - ord('a')] -= 1
-        
+            charHash[t_lower[i]] = charHash.get(t_lower[i], 0) - 1
+
         for charKey in charHash:
-            if charKey != 0:
+            if charHash[charKey] != 0:
                 return False
-        
+
         return True
 
-        '''
+        """
         In this version, converted string to lowercase and then used a simgle
         array, initialzed to all 0's to track occurences of each char in either str.
         If we run into it in s1, +=1
@@ -27,4 +27,4 @@ class Solution:
         At the end, if the char Hash array has any value != 0, something didn't
         match between strings. (Since if they had equal occurences of each char,
         after all inc and decr, val should be 0)
-        '''
+        """
