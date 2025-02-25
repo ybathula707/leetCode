b1b086fd3b4396a309d9e36cabdd8a9660f9f638
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2025-02-24 23:40:35 -0800

    Runtime: 15 ms (43.42%), Memory: 17.6 MB (97.44%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -2,19 +2,25 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): 
             return False
-        
-        s_lower = s.lower()
-        t_lower = t.lower()
 
-        charHash = [0] * 26
+        charHash1, charHash2 = {}, {}
 
-        for i in range(len(s_lower)):
-            charHash[ord(s_lower[i]) - ord('a')] += 1
-        for i in range(len(t_lower)):
-            charHash[ord(t_lower[i]) - ord('a')] -= 1
+        '''
+         Hash map will be key:value pairs where
+          key is a character: val is counter 
+         
+         Each string will have its own hashed characters,
+         which on comparison, we can return False if there 
+         is not a match. 
+         -> Means that the strings did not have identical characters
+        O(n + m) space and O(n) time
+        '''
+        for i in range(len(s)):
+            charHash1[s[i]] = 1 + charHash1.get(s[i], 0)
+            charHash2[t[i]] = 1 + charHash2.get(t[i], 0)
         
-        for charKey in charHash:
-            if charKey != 0:
+        for charKey in charHash1:
+            if charHash1[charKey] != charHash2.get(charKey, 0):
                 return False
         
         return True
