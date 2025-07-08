282e7506141ca7d248fe503b10cbd14a249b53b5
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2025-07-07 23:17:53 -0700

    Runtime: 3 ms (50.41%), Memory: 19.1 MB (23.86%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,27 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return []
-        if len(nums) == 0:
-            return []
-        # Base cases of empty list or NULL list
+        '''
+        Iterate nums using indexes
+        In hash map, if complemet does not exist, insert into map
+        Index hashMap as {value: index}
+        IN this way, when we encounter map[value] == complement,
+        we can simply return
 
-        hashMap = {}
+        map[value], map[target]
+        which will each hold the index
+        '''
+        #Base cases where array is empty or NULL (return [])
+        if nums is None or len(nums) == 0:
+            return []
+
+        hashMap ={}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hashMap:
                 return [i, hashMap[complement]]
             hashMap[nums[i]] = i
-        
-        #empty list if no pair is found
-        return []
 
-        """
-        Idea: 
-            - have a hash map where key:val pairs are num:idx.
-            - for each element, check if complement is in the map before
-            inserting.
-             Return the indexes if found
-             else [] empty array
-            is returned
-        """
+        return [] # no teo pair found 
+      
+        
