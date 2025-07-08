fe3596f372993de16f7860f68e5660835c6cceb6
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2025-07-07 22:56:03 -0700

    Runtime: 3 ms (50.41%), Memory: 18.9 MB (50.78%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,40 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
+        if not nums:
+            return []
+        if len(nums) == 0:
+            return []
+        # Base cases of empty list or NULL list
 
+        hashMap = {}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hashMap:
-                return [hashMap[complement], i]
+                return [i, hashMap[complement]]
             hashMap[nums[i]] = i
         
+        #empty list if no pair is found
         return []
 
-    """
-    Given an array we need to return indices of two values
-    that sum to a target
-   
-    NOTE: NOT sameas sort => sum pairs to return BOOL or the 
-    actual value. We need to mainitin index of each val, to return
-        -- this is a teo pointer, since we sort
-
-
-    Strategy: 
-
-    BF: for every elemnt i in the array, do the next
-    values equal that? If so, return index as we check. 
-
-    KEY: we do not need to know rest of the array
-    in order to determine the COMPLEMENT of nums[i] that
-    would give us the target.
-
-    I.e if target is 10, and nums[3] is 4, we know that
-    6 is the value we need for a solution
-        => optimize how we search for 6 and index of 
-        6 within this array
-
-
-
-
-
-    """
+        """
+        Idea: 
+            - have a hash map where key:val pairs are num:idx.
+            - for each element, check if complement is in the map before
+            inserting.
+             Return the indexes if found
+             else [] empty array
+            is returned
+        """
