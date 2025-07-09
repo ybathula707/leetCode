212f324dc4c4fc41b835dedd614c0f86344e7bc3
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2025-07-08 23:29:48 -0700

    Runtime: 552 ms (71.33%), Memory: 20.7 MB (36.73%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -13,9 +13,16 @@ class Solution:
                 currentSum = nums[i] + nums[left] + nums[right]
                 # updating pointers based on current sum w.r to 0
 
+                # use if/ elif and else block for 3 seperate cases.
+                # if we did if, if and else, then the logic is WRONG
+                # why? cuz else is part of the second if statement, they are not all grouped togehter
+                # This means tuple is appended only if currSum <0, which is not the behavior we are going for
+                # We want 3 seperate checks, where in each loop only one executes.
+                # for that, if/elif and else is the correct choice.
+
                 if currentSum > 0:  # decrement the larger pointer
                     right -= 1
-                elif currentSum < 0:   # increment the smaller pointer
+                elif currentSum < 0:   # increment the smaller pointer 
                     left += 1
                 else:
                     # append the items into array since they equal 0
