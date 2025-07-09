c0d564731e0d619782fecd0b7aa236530080e229
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2025-07-09 14:40:08 -0700

    Runtime: 7 ms (90.13%), Memory: 18.4 MB (91.85%) - Gitcode

--- 345-reverse-vowels-of-a-string/345. Reverse Vowels of a String.py
+++ 345-reverse-vowels-of-a-string/345. Reverse Vowels of a String.py
@@ -1,25 +1,22 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-         # Define a set of vowels for quick lookup
-        vowels = "aeiouAEIOU"
-    
-        # Convert the string to a list to modify it easily
-        s_list = list(s)
-    
-        # Initialize pointers for the start and end of the string
-        left, right = 0, len(s) - 1
-    
-        # Loop until the two pointers meet or cross each other
+        vowels='aeiouAEIOU'
+        stringList = list(s)
+        # convert input string to list to modify it easily
+        # we'll need to convert back to string before returning
+
+        left, right = 0, len(stringList)-1
+        #pointers init to left and right of string array
+
         while left < right:
-            # Move the left pointer to find a vowel on the left side
-            if s_list[left] not in vowels:
+            #increment both ptrs until both pointing to vowels
+            if stringList[left] not in vowels:
                 left += 1
-            # Move the right pointer to find a vowel on the right side
-            elif s_list[right] not in vowels:
+            elif stringList[right] not in vowels:
                 right -= 1
-            # If both pointers have found vowels, swap them and move both pointers
             else:
-                s_list[left], s_list[right] = s_list[right], s_list[left]
+                stringList[left], stringList[right] = stringList[right], stringList[left]
+                # swapping reverseVowels
                 left += 1
                 right -= 1
-        return ''.join(s_list)
+        return ''.join(stringList)
