6132b6cec1c0cb46599c04cc711517bb733cbb7f
Yoshi <60422402+ybathula707@users.noreply.github.com>
2026-04-05 16:20:37 -0700

    Runtime: 619 ms (64.99%), Memory: 22.3 MB (64.00%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -1,41 +1,49 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        soltn_array =[]
         nums.sort()
-        retArray = []
 
-        for i in range(len(nums) - 2):
-            # keep incrementing first tuple item intil not duplicate
-            if i > 0 and nums[i - 1] == nums[i]:
+        # range from i -> 2nd to last element
+        for current in (range(len(nums) - 2)):
+            if current > 0 and nums[current - 1] == nums[current]:
                 continue
-            left = i + 1
+            left = current + 1
             right = len(nums) - 1
+
+            #while left and right dont overlap, run sum checks
             while left < right:
-                currentSum = nums[i] + nums[left] + nums[right]
-                # updating pointers based on current sum w.r to 0
-
-                # use if/ elif and else block for 3 seperate cases.
-                # if we did if, if and else, then the logic is WRONG
-                # why? cuz else is part of the second if statement, they are not all grouped togehter
-                # This means tuple is appended only if currSum <0, which is not the behavior we are going for
-                # We want 3 seperate checks, where in each loop only one executes.
-                # for that, if/elif and else is the correct choice.
-
-                if currentSum > 0:  # decrement the larger pointer
-                    right -= 1
-                elif currentSum < 0:   # increment the smaller pointer 
+                current_sum = nums[current] + nums[left] + nums[right]
+                if current_sum > 0:
+                    right -=1
+                elif current_sum < 0:
                     left += 1
                 else:
-                    # append the items into array since they equal 0
-                    retArray.append([nums[i], nums[left], nums[right]])
-                    # incrment left and right while pointers dont overlap 
-                    # and while next left or right is dup to current pointer
-                    while left < right and nums[left + 1] == nums[left]:
-                        left += 1
-                    while left < right and nums[right - 1] == nums[right]:
+                    soltn_array.append([nums[current], nums[left], nums[right]])
+                    while left < right and nums[right -1 ] == nums[right]:
                         right -= 1
-                    # increment left and right arbitrarily, or one final time after 
-                    # avoiding dup logic
+                    while left < right and nums[left + 1 ] == nums [left]:
+                        left += 1
                     left += 1
-                    right -= 1
+                    right -= 1 
 
-        return retArray
+
+        return soltn_array
+
+
+
+
+
+
+
+
+
+    ''' 
+        Idea:
+        - sort arraty
+        - 2 ptr approach will be left and right ptr where left is curr +1 
+        - curr is index i
+
+        Incr scenarios:
+
+
+    '''
