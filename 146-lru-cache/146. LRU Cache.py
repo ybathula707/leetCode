3079914f7bd7a8877bb6e8007d36105382df0d06
Yoshi <60422402+ybathula707@users.noreply.github.com>
2026-04-07 13:18:14 -0700

    Runtime: 149 ms (24.50%), Memory: 78.8 MB (18.79%) - Gitcode

--- 146-lru-cache/146. LRU Cache.py
+++ 146-lru-cache/146. LRU Cache.py
@@ -1,28 +1,93 @@
-class LRUCache:
+#Creating the Node Class
 
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        # key is the access order of this cache items (index in the LL)
+
+        self.val = value
+        # value of the node
+
+        self.next = None
+        self.previous = None
+
+
+class LRUCache:
+    # doubly linked list w/ hash map for O(1) lookups
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = collections.OrderedDict()
-        #orderedDictionaries already store items in order
-        #no need to declare LL since this ds handles ordering + lookups
-        
-    # if in cache -> return and move to end. Else ret -1
+
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+
+        self.head.next = self.tail
+        # new list head points to tail
+        self.tail.previous = self.head
+        # new list tail prev ref to head
+
+        self.cache_keys = {} 
+        # will contain key:node mapping of nodes in Cache LL to give O(1) lookups
+
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        '''
+            Return node if hashmap contains it, move node to end.
+            Return -1 otherwise
+
+        '''
+        if key not in self.cache_keys:
             return -1
-        
-        self.cache.move_to_end(key)
-        return self.cache[key] # returns node val @ key 
-        
-    # if in cache -> move to end & update put val. Pop last item if full
+
+        node = self.cache_keys[key]
+        self.delete(node)
+        self.add_to_end(node)
+        return node.val
+
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-        
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(False)
-        
+        '''
+            If node already exists, remove node and add to end.
+            If at capacity, remove node at head before adding to end
+        '''
+        if key in self.cache_keys:
+            old_node = self.cache_keys[key]
+            self.delete(old_node)
+
+        node = ListNode(key, value)
+        self.cache_keys[key] = node
+        self.add_to_end(node)
+
+        # check capacity and evict head (oldest) if reached
+        # evict --> 1) delete node from LL 2)delete key from cache key hash
+        if len(self.cache_keys) > self.capacity:
+            node_delete = self.head.next
+            self.delete(node_delete)
+            del self.cache_keys[node_delete.key]
+
+
+
+    # Insert node to end of the list (inserting at end give us O(1) time complexity)
+    def add_to_end(self, node: ListNode):
+        ''' 
+            1. track the end of LL (tail.previous)
+            2. set tail.pervious.next to new node 
+            3. set new node.previous to previous
+            4. set new node.next to tail
+            5. set tail.previous to new node
+        '''
+        current_end = self.tail.previous
+        current_end.next = node
+        node.previous = current_end
+        node.next = self.tail
+        self.tail.previous = node
+
+
+    # delete node 
+    def delete(self, node: ListNode):
+        '''
+            Delete current node by setting previous and next pointers
+        '''
+        node.previous.next = node.next
+        node.next.previous = node.previous
+
 
 
 # Your LRUCache object will be instantiated and called as such:
