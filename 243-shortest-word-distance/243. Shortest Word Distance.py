644c8b54130b6450cf2e87135d394eec1ae59726
Yoshi <60422402+ybathula707@users.noreply.github.com>
2026-04-07 22:36:30 -0700

    Runtime: 3 ms (45.41%), Memory: 21.3 MB (91.44%) - Gitcode

--- 243-shortest-word-distance/243. Shortest Word Distance.py
+++ 243-shortest-word-distance/243. Shortest Word Distance.py
@@ -1,34 +1,20 @@
 
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        wordsIndex = {}
+        # iterate only once, assigning ptr1 to first found word
+        # asignt ptr 2 in whichever iteration you could find it
+        # this first time these pointers are both assigned, we calculate the
+        # difference and can return this. 
+        p1, p2 = -1, -1
+        minDif = float("inf")
 
-        if not word1 or not word2:
-            return None
+        for i in range(len(wordsDict)):
+            if wordsDict[i] == word1:
+                p1 = i
+            if wordsDict[i] == word2:
+                p2 = i
 
-        #O(n) to populate but O(1) lookups
-        for i, word in enumerate(wordsDict):
-            if word not in wordsIndex:
-                wordsIndex[word] = []
-            wordsIndex[word].append(i)
+            if p1 != -1 and p2 != -1:
+                minDif = min(minDif, abs(p2-p1))
 
-        if word1 not in wordsIndex or word2 not in wordsIndex:
-            return None
-
-        # load the word index arrays
-        word1IndexArray = wordsIndex[word1]
-        word2IndexArray = wordsIndex[word2]
-
-        # init ptrs and min aggregator
-        word1Ptr, word2Ptr, minDist = 0, 0, float("inf")
-
-        while word1Ptr < len(word1IndexArray) and word2Ptr < len(word2IndexArray):
-            minDist = min(minDist, abs(word1IndexArray[word1Ptr] - word2IndexArray[word2Ptr]))
-
-            # move the pointer that's the smaller index val
-            if word1IndexArray[word1Ptr] < word2IndexArray[word2Ptr]:
-                word1Ptr += 1
-            else:
-                word2Ptr += 1
-
-        return minDist
+        return minDif
