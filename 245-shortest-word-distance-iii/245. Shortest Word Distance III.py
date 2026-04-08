6eaf8270ead71b164c18ac8225fc0ca590675d5b
Yoshi <60422402+ybathula707@users.noreply.github.com>
2026-04-07 19:54:37 -0700

    Runtime: 83 ms (13.82%), Memory: 38.2 MB (7.02%) - Gitcode

---
+++ 245-shortest-word-distance-iii/245. Shortest Word Distance III.py
@@ -0,0 +1,41 @@
+class Solution:
+    def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        wordsIndex = {}
+
+        if not word1 or not word2:
+            return None
+
+        #O(n) to populate but O(1) lookups
+        for i, word in enumerate(wordsDict):
+            if word not in wordsIndex:
+                wordsIndex[word] = []
+            wordsIndex[word].append(i)
+
+        if word1 not in wordsIndex or word2 not in wordsIndex:
+            return None
+
+        #if same word, we look for min in the same array
+        if word1 == word2:
+            indeces = wordsIndex[word1]
+            min_diff = float("inf")
+            for i in range(1, len(indeces)):
+                min_diff = min(min_diff, abs(indeces[i-1] - indeces[i]))
+            return min_diff
+    
+        # load the word index arrays
+        word1IndexArray = wordsIndex[word1]
+        word2IndexArray = wordsIndex[word2]
+
+        # init ptrs and min aggregator
+        word1Ptr, word2Ptr, minDist = 0, 0, float("inf")
+     
+        while word1Ptr < len(word1IndexArray) and word2Ptr < len(word2IndexArray):
+            minDist = min(minDist, abs(word1IndexArray[word1Ptr] - word2IndexArray[word2Ptr]))
+
+            # move the pointer that's the smaller index val
+            if word1IndexArray[word1Ptr] < word2IndexArray[word2Ptr]:
+                word1Ptr += 1
+            else:
+                word2Ptr += 1
+
+        return minDist
