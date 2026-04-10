0ab59e128a08ad1bc1dd0f469858f91bec9a51eb
Yoshi <60422402+ybathula707@users.noreply.github.com>
2026-04-09 22:53:10 -0700

    Runtime: 19 ms (22.23%), Memory: 22.8 MB (38.81%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -22,7 +22,6 @@ class Solution:
                 This way, we can jsut print the values of the hashmap for
                 a grouped list of anagram strings
 
-            * Note we cannot use list as a key, so convert the character 
-            count hash per string
+            * Note we cannot use list as a key, so convert the character count hash per string
             into a tuple before inseritign
         """
