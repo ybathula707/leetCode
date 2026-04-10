808a2b47584d2a28a3d4f2fe913c1ebf8c9d77cf
Yoshi <60422402+ybathula707@users.noreply.github.com>
2026-04-09 22:19:32 -0700

    Runtime: 11 ms (65.68%), Memory: 36.3 MB (11.53%) - Gitcode

--- 217-contains-duplicate/217. Contains Duplicate.py
+++ 217-contains-duplicate/217. Contains Duplicate.py
@@ -1,7 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        numsSet = set()
-        numsSet.update(nums)
+        if nums is None:
+            return False
 
-        return len(numsSet) < len(nums)
-             
+        if len(nums) == 0 or len(nums) == 1 :
+            return False
+
+        unique_nums = {}
+    
+        for num in nums:
+            if num in unique_nums:
+                return True
+            unique_nums[num] = num
+        
+        return False
